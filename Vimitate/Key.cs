10c7ab142bb12dbc1b1699e53d9955631045b784
Alex Novak <alexwnovak@gmail.com>
2022-08-07 10:48:09 -0400

    +Key enum

---
+++ Vimitate/Key.cs
@@ -0,0 +1,46 @@
+namespace Vimitate;
+
+/// <summary>
+/// Specifies a keyboard key.
+/// </summary>
+public enum Key
+{
+    None,
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+    D0,
+    D1,
+    D2,
+    D3,
+    D4,
+    D5,
+    D6,
+    D7,
+    D8,
+    D9,
+    Escape,
+}
