f7175d379149be42805586153f7dd9903e2ffd47
Alex Novak <alexwnovak@gmail.com>
2022-08-07 14:18:52 -0400

    +Global usings, include an alias for Feature

--- src/VimitateTests/GlobalUsings.cs
+++ src/VimitateTests/GlobalUsings.cs
@@ -1,4 +1,6 @@
 global using System;
 global using Vimitate;
 global using Xunit.Gherkin.Quick;
+global using Gherkin.Ast;
 global using FluentAssertions;
+global using Feature = Xunit.Gherkin.Quick.Feature;
