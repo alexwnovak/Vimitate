e6c2104fb8fbe57d760db23450bbe8b387b85a62
Alex Novak <alexwnovak@gmail.com>
2022-08-07 11:04:37 -0400

    Stubbing out the basic API

--- Vimitate/VimEngine.cs
+++ Vimitate/VimEngine.cs
@@ -2,4 +2,14 @@ namespace Vimitate;
 
 public class VimEngine
 {
+    private string _text = "";
+
+    public State GetState()
+    {
+        return new(_text);
+    }
+
+    public void KeyPress(Key key)
+    {
+    }
 }
